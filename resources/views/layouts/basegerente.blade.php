b59c2deb8a49aedb76e17279f239ed9d494c38bd
peterosee <peterosee@gmail.com>
2020-10-28 15:46:29 -0600

    correcion clientes y membresia

--- resources/views/layouts/basegerente.blade.php
+++ resources/views/layouts/basegerente.blade.php
@@ -4,19 +4,43 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
 
   <title>GOKAart----</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- IonIcons -->
-  <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <link rel="stylesheet" href="{{asset('dist/css/efecto.css')}}">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- daterange picker -->
+  <link rel="stylesheet" href="../../plugins/daterangepicker/daterangepicker.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="../../plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="../../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="../../plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="../../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <!-- Bootstrap4 Duallistbox -->
+  <link rel="stylesheet" href="../../plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -29,10 +53,10 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="/inicio" class="nav-link">Ruta66</a>
+        <a href="/" class="nav-link">Ruta66</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="/inicio" class="nav-link"></a>
+        <a href="/" class="nav-link"></a>
 
       </li>
 
@@ -79,23 +103,20 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="/inicio" class="brand-link">
-      <img center src="{{asset('dist/img/LOGO2.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-10"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">.</span>
+    <a href="/" class="" style="height:80px" center>
+      <div class="mt-4" align="center">
+      <img center src="{{asset('dist/img/LOGO2.png')}}" alt="Logo Ruta66" class="img-fluid"
+           style="opacity: .8;
+           height:70px;
+           width:90px;
+           border-radius: 8px;
+           center center;" >
+         </div>
     </a>
-
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
-
-        </div>
-        <div class="info">
-          <a class="d-block">{{ Auth::user()->name }}</a>
-        </div>
       </div>
 
       <!-- Sidebar Menu -->
@@ -114,6 +135,17 @@
                 </p>
               </a>
             </li>
+            <li class="nav-item">
+              <a href="/impresionResponsiva" class="nav-link">
+                <i class="nav-icon fas fa-file-medical-alt"></i>
+                <p>
+                  Responsiva
+                  <span class="badge badge-info right"></span>
+                </p>
+              </a>
+            </li>
+            </li>
+@if(Auth::user()->hasRole('admin')==1||Auth::user()->hasRole('gerente')==1)
             <!--Prueba de Recargas-->
             <li class="nav-item">
               <a href="/recargas" class="nav-link">
@@ -124,7 +156,7 @@
                 </p>
               </a>
             </li>
-            </li>
+          </li>
 
             <!--fin de Recargas-->
 
@@ -132,6 +164,7 @@
           <!--Ventas-->
 
           <!--FIN Ventas-->
+
 
 
           <li class="nav-header">FINANZAS</li>
@@ -145,7 +178,7 @@
             </a>
           </li>
 
-         
+
           <li class="nav-header">ADMINISTRACIÓN</li>
           <li class="nav-item">
             <a href="/indexcliente" class="nav-link">
@@ -167,6 +200,18 @@
             </a>
           </li>
 
+          <!--  -->
+          <li class="nav-item">
+            <a href="/ventas" class="nav-link">
+            <i class="nav-icon fas fa-comments-dollar"></i>
+              <p>
+                Ventas
+                <span class="badge badge-info right"></span>
+              </p>
+            </a>
+          </li>
+
+
           <li class="nav-item">
             <a href="/indexmembresia" class="nav-link">
             <i class="nav-icon fas fa-address-card"></i>
@@ -177,6 +222,7 @@
             </a>
           </li>
 
+          @if(Auth::user()->hasRole('admin')==1)
           <li class="nav-item">
             <a href="/indextrabajador" class="nav-link">
             <i class="nav-icon fas fa-chalkboard-teacher"></i>
@@ -186,7 +232,8 @@
               </p>
             </a>
           </li>
-
+          @endif
+@endif
           <!--MISCELANEA REVISAR-->
 
         </ul>
@@ -225,8 +272,9 @@
     <!--=========================FIN DE BOTONES======================-->
 
 
-
     @section('contenido')
+
+
     @show
 
 
@@ -257,6 +305,7 @@
 
 <!-- jQuery -->
 <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="../../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
 <!-- Bootstrap -->
 <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE -->
@@ -266,7 +315,34 @@
 <script src="{{asset('plugins/chart.js/Chart.min.js')}}"></script>
 <script src="{{asset('dist/js/demo.js')}}"></script>
 <script src="{{asset('dist/js/pages/dashboard3.js')}}"></script>
-
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Select2 -->
+<script src="../../plugins/select2/js/select2.full.min.js"></script>
+<!-- Bootstrap4 Duallistbox -->
+<script src="../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<!-- InputMask -->
+<script src="../../plugins/moment/moment.min.js"></script>
+<script src="../../plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+<!-- date-range-picker -->
+<script src="../../plugins/daterangepicker/daterangepicker.js"></script>
+<!-- bootstrap color picker -->
+<script src="../../plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Bootstrap Switch -->
+<script src="../../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../../dist/js/pages/dashboard3.js"></script>
 
 </body>
 </html>
